633dca75c2e907d3982a78fab67b0951eb0758cf
mbatorek7 <maegan.batorek@valpo.edu>
2025-06-23 15:09:36 -0500

    store to do list in browser

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 
 const Todo = () => {
 
-    const [todoList, setTodoList] = useState([]);
+    const [todoList, setTodoList] = useState(localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : []);
 
     const inputRef = useRef();
 
@@ -43,7 +43,7 @@ const Todo = () => {
     }
 
     useEffect(() => {
-        console.log(todoList);
+        localStorage.setItem("todos", JSON.stringify(todoList))
     }, [todoList])
 
     return (
