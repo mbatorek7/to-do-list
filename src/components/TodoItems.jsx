ea07183f44adffe22675c237961539b417e0476a
mbatorek7 <maegan.batorek@valpo.edu>
2025-06-23 14:58:56 -0500

    line through completed tasks

--- src/components/TodoItems.jsx
+++ src/components/TodoItems.jsx
@@ -8,7 +8,7 @@ const TodoItems = ({ text, id, isComplete, deleteTodo, toggle }) => {
         <div className='flex items-center my-3 gap-2'>
             <div onClick={() => { toggle(id) }} className='flex flex-1 items-center cursor-pointer'>
                 <img className='w-7' src={isComplete ? tick : not_tick} alt="" />
-                <p className='text-slate-700 ml-4 text-[17px]'>
+                <p className={`text-slate-700 ml-4 text-[17px] ${isComplete ? "line-through" : ""}`}>
                     {text}
                 </p>
             </div>
